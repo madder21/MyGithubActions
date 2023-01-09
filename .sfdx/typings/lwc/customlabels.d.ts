6fe633a078d0231a5cf790bd91fa292bb934ee67
RSORimowa <rabi.soussi@viseo.com>
2023-01-09 18:25:19 +0100

    first commit

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/label/c.BL_AppSettings_Instantiation_Error" {
+    var BL_AppSettings_Instantiation_Error: string;
+    export default BL_AppSettings_Instantiation_Error;
+}
