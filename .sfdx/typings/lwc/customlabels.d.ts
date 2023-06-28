660c688c26d107ea9e6b78f9021745d5d4fbfde7
mohamed.abdelfadel <mohamed.abdelfadel@viseo.com>
2023-06-28 11:42:00 +0200

    delete files

--- .sfdx/typings/lwc/customlabels.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "@salesforce/label/c.BL_AppSettings_Instantiation_Error" {
-    var BL_AppSettings_Instantiation_Error: string;
-    export default BL_AppSettings_Instantiation_Error;
-}
